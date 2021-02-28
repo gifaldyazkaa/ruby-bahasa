8120061af3712b89a4f151b41b47aca5dfe981ae
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 19:52:46 +0700

    Create latihan6.rb

---
+++ 06 - Return/latihan6.rb
@@ -0,0 +1,47 @@
+# Latihan 6 - Return
+
+# Seperti yang dilihat, Return adalah Kembali, Back. Kembalian.
+
+# Jadi, fungsi ini berguna untuk mengembalikan nilai. Mengembalikan ini belum berarti kita print, atau kita tampilakan hasilnya. Jadi dia bener bener cuma mengembalikan doang.
+
+# Masalah hasil, gimana kita, kita mau apain, atau mau operasiin, atau print. Itu bebas dari kita.
+
+# Kita mulai panggil fungsi
+
+# Dengan nama-nya "Jumlah" dalam lowercase.
+
+def jumlah(num1, num2)
+
+    num = num1 + num2
+
+    return num
+
+end
+
+# coba kita panggil fungsinya.
+
+# jumlah(10, 20)
+
+# Hasilnya kosong. Kok Bisa? Karena kita baru memanggil doang, belum dengan nge-print atau puts nya.
+
+# Jadi Return ini bener bener mengembalikan nilai. Kita bebas mau apakan.
+
+# Untuk Nge-print, sama aja. Kita harus panggil PUTS.
+
+# puts jumlah(10, 20)
+
+# Jadi sama seperti di Latihan 5. Kita hanya menyimpan di variabel. dan ketika kita panggil, hasinya tidak langsung muncul. karena butuh PUTS atau PRINT.
+
+# Tapi FYI, Kita bisa olah lagi hasilnya ini.
+
+# Caranya?
+
+hasil = jumlah(10, 20)
+
+puts hasil - 30
+
+# Nah. Jadi 0 kan! Kelakuan siapa tuh! Ngaku hayo! Jelas karena kita olah ulang. 30 - 30 = 0.
+
+# Semoga tidak Pusing Bro.
+
+# Semoga Bermanfaat.
