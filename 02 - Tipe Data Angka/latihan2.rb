7a8c1025ee42e6862a22bc93192b8af1b1c57f9c
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 19:27:15 +0700

    Create latihan2.rb

---
+++ 02 - Tipe Data Angka/latihan2.rb
@@ -0,0 +1,23 @@
+# Latihan 2 - Tipe Data Angka (Number).
+
+# Variabel dalam ruby dan cara menggunakan variabel
+
+    a = 20
+
+    b = 30
+
+    c = "Tau!"
+
+# Kita coba operasi matematika.
+
+    # puts "Hasil Var A || Var B =" + a + b
+
+# Oh! Cara diatas salah! Ada Errornya! Kalau gitu coba tambahkan #{}.
+
+    puts "Hasil Var A || Var B = #{a + b}"
+
+# Berhasil! Sekarang kita masukkan Variabel C yang isinya "Tau!" yuk!!!
+
+    puts "Hasil Var A || Var B = #{a + b} #{c}"
+
+# Oke pak! Berhasil! Kalau gak percaya Yang Cara Pertama Salah? Un-comment aja dengan cara hilangkan tanda # diawal.
