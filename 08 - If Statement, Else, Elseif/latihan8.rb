b1776fee2438e6ccfb79a4f0553e5794ce6839cc
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 19:59:06 +0700

    Create latihan8.rb

---
+++ 08 - If Statement, Else, Elseif/latihan8.rb
@@ -0,0 +1,33 @@
+# Latihan 8 - If Statement, Else, Elseif
+
+# Statemen If dipakai ketika kita menanyakan apakah ini benar? (Ini semacam pemeriksaan).
+
+# Kita lihat contohnya.
+
+angka = 10
+
+if angka == 10
+
+    puts "Angkanya 10"
+
+elsif angka == 100
+
+    puts "Angkanya 100"
+
+else
+
+    puts "Angkanya bukan 10 dan 100."
+
+end
+
+# Nah, cobalah untuk mengubah variabel angka dari 10 Jadi 100. Lalu Save.
+
+# Apa yang terjadi?
+
+# Buktikan sendiri :3
+
+# Intinya itulah If Statement, Else, Elseif.
+
+# Kalau Pusing, Jangan ragu, Cukup Bertanya di Pull Request.
+
+# Good Luck, Semoga Bermanfaat.
