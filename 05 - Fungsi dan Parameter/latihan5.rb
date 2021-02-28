74046e90cff069d2faf6eb7ce1f3c2b5bad474a6
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 19:50:25 +0700

    Create latihan5.rb

---
+++ 05 - Fungsi dan Parameter/latihan5.rb
@@ -0,0 +1,73 @@
+# Latihan 5 - Fungsi dan Parameter
+
+# Nah, Fungsi dan Parameter ini Penting gunanya di Bahasa Pemrograman
+
+# Fungsi = Kita memberitahu program untuk mempersiapkan/melakukan sesuatu hal tertentu. Tapi tidak otomatis. Dia akan melakukan ketika kita menyuruhnya.
+
+# Kalau masih pusing, gak apa bro, simak dulu yuk!
+
+#
+
+# Function
+
+# DEF - Definisi / Definition / Define
+
+def jalan()
+
+    puts "Majuuuu! Jalan!!!"
+
+end
+
+# Seperti itu contohnya. Kalau kita Run di Terminal, Gak akan ada hasil, karena kita baru mulai.
+
+# Jadi anggap saja, kalau yang diatas itu sama seperti variabel. ktia baru meyiapkan. belum menyuruhnya.
+
+#
+
+# Cara Men-jalankan nya gimana Fal?
+
+# Sebutkan JALAN nya di sini
+
+jalan()
+
+# Jangan Lupa pakai kurung.
+
+#
+
+# Nahh kan, ada gitu Hasilnya. Ketika kita suruh, Programnya akan Jalan.
+
+# Jadi, Begitulah...
+
+#
+
+# Lengkapnya, Def adalah Definisi.
+
+# Kita mau mendefinisikan kalau kita menyebut namanya (di projek ini saya pakai nama "jalan") dalam program, maka program akan melakukan hal yang sudah disiapkan kalian.
+
+# Jadi ini (mungkin) sama seperti variabel. Kita menyimpan dulu perintah. Tapi, Ketika kita panggil namanya, maka Perintah akan Dilaksanakan.
+
+# Parameter gimana pak?
+
+# Oksokslah Brok.
+
+# Kita gunakan def lagi untuk parameter.
+
+# Parameter
+
+def say(num)
+
+    puts "Angka yang di-atur adalah : #{num}"
+
+end
+
+# Kita panggil namanya.
+
+say(100)
+
+#    ^------------------------------------|
+
+# Nah, seperti yang dilihat di Terminal, k|ok angkanya seratus? kok segini? kok Angka yang disebutkan bla-bla-bla?
+
+# Soal itu kamu bisa ganti di atas sini---|.
+
+# Jadi Begitu kalau Soal Parameter :)
