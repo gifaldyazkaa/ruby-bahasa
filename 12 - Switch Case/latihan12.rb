4b233283d5a9dd9456111bb617ac384627e5e907
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 20:05:23 +0700

    Create latihan12.rb

---
+++ 12 - Switch Case/latihan12.rb
@@ -0,0 +1,53 @@
+# Latihan 12 - Switch Case
+
+# Nah, Sebelum sebelumnya kita sudah belajar If, Elseif, Else.
+
+# Kalau kita mau membuat banyak pengujian, otomatis kita harus menulis banyak elseif, else.
+
+# Namun, Dengan Switch Case, Kita akan dimudahkan jika ingin membuat banyak pengecualian.
+
+# Langsung saja yuk.
+
+# Silahkan ubah Variabel angka dari 1-3. Yang akan muncul di Terminal pun akan berbeda.
+
+# Kalau angkanya lebih dari 3? Coba aja deh.
+
+angka = 2
+
+case angka
+
+when 1
+
+    puts "Angka Nya 1."
+
+when 2
+
+    puts "Angka Nya 2."
+
+when 3
+
+    puts "Angka Nya 3."    
+
+else
+
+    puts "Tidak memenuhi syarat angka."
+
+end
+
+# Diatas, ada when 1, when 2, when 3. Nah fungsinya buat apa kak?
+
+# When kan Bahasa Inggris. Artinya apa hayo?
+
+# Ketika.
+
+# Jadi, when variabel angka = 1, 2, atau 3,
+
+# Lakukan hal ini.
+
+# Jadi tidak perlu repot repot menuliskan banyak elsif.
+
+# Begitu....
+
+# Semoga tidak pusing. Kalau pusing Tinggal Bertanya di Pull Requests.
+
+# Semoga Bermanfaat.
