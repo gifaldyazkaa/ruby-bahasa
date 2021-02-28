ae32ab4cb57ec831913733348badd554229282c0
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 19:21:35 +0700

    Add files via upload

---
+++ 00 - Tipe Data String/latihan1.rb
@@ -0,0 +1,15 @@
+# Latihan Menulis Hello World (String).
+
+# Cara Pertama - Pakai PUTS
+puts "Hello World"
+
+# Cara kedua - Pakai VARIABEL
+halo = "Hello World"
+puts halo
+
+# Cara Ketiga - Pakai PRINT
+print halo
+
+# Atau
+puts ""
+print "Hello World"
