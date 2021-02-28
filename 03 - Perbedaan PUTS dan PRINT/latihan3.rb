c4805588b6285a550d311bded5d63a2da9ed13d8
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 19:46:09 +0700

    Create latihan3.rb

---
+++ 03 - Perbedaan PUTS dan PRINT/latihan3.rb
@@ -0,0 +1,35 @@
+# Latihan 3 - Perbedaan PUTS dan PRINT di Ruby.
+
+# Kalau pakai PUTS
+
+var1 = "Halo"
+
+var2 = "HeyHey"
+
+puts "Ini Pakai PUTS : "
+
+puts var1
+
+puts var2
+
+# Di Terminal, kamu akan melihat bahwa Halo dan HeyHey ada di baris Berbeda.
+
+# Sedangkan kalau pakai PRINT
+
+puts ""
+
+print "Ini pakai PRINT : "
+
+print var1
+
+print var2
+
+puts ""
+
+# Nahh kan, kalau pakai PRINT, Halo dan HeyHey akan bersatu di baris yang sama.
+
+# Jadi, Itulah perbedaannya. Pakailah kedua sintaks ini sesuai kebutuhan.
+
+# Kalau lagi butuhnya String Berpisah Baris, Pakai PUTS
+
+# Kalau butuhnya String Menyatu di Satu Baris, Pakai PRINT.
