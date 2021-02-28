63b20ab02a5a07ae94539c57e128fad6b71f5c4e
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 20:06:56 +0700

    Create latihan13.rb

---
+++ 13 - Array/latihan13.rb
@@ -0,0 +1,77 @@
+# Latihan 13 - Tipe Data Array
+
+# Wuohh Pak, Kita dah masuk Array pak:D Siap siap pusing nih:D wkwkwk
+
+# Jadi Array ini seperti Kotak Kotak / Lemari. Dimana kita bisa menyimpan beberapa data dalam 1 set.
+
+# Misalnya, kita mau menyimpan angka dalam 1 variabel
+
+angka = [1, 2, 3, 4, 5]
+
+# Lahh pak, kok isinya 5?!
+
+# Karena diawali [], maka program akan menganggap itu array dan seperti penjelasan diatas.
+
+# Anngaplah variabel angka ini Lemari. Kita gak mau di dalam lemari cuma ada satu baju doang kan?
+
+# Kita pakai lah array untuk menyimpan beberapa baju (data) ke lemari (variabel).
+
+# Cara nge-print nya seperti biasa.
+
+    # puts angka
+
+    
+
+    # *UNCOMMENT (HAPUS #) KALAU MAU LIHAT HASIL.    
+
+# Kita coba dengan String.
+
+bahasa = ["Java", "JavaScript", "Ruby", "HTML", "CSS", "Python"]
+
+    # puts bahasa
+
+    # *UNCOMMENT (HAPUS #) KALAU MAU LIHAT HASIL.
+
+# Nah kan? Sama yak...
+
+# Kalau mau ngambil baju (data) yang pertama aja, bisa gak pak? JELAS BISA...
+
+# Misalnya kita mau ambil Java doang.
+
+puts ""
+
+puts "Mengambil Data Array : "
+
+puts ""
+
+puts "Data Pertama : " + bahasa.first
+
+# Nah kan. Bisa Pak. Pakai metode ini pak kalo mau ambil data pertama doang.
+
+# Kalo mau yang terakhir tinggal pakai last.
+
+puts "Data Terakhir : " + bahasa.last
+
+# Begitu.
+
+# Kalau mau ambil dua data? Gimana Pak?
+
+# Gini....
+
+puts "Dua Data Sekaligus : #{bahasa.take(2)}"
+
+# Kalau mau tau ada berapa data di dalam Array? Begini Juga pak...
+
+puts ""
+
+puts "Jumlah Array : #{bahasa.count}"
+
+puts "Length Array : #{bahasa.length}"
+
+# Pilihlah Salah satu metode diatas kalau mau mengetahui Jumlah array. Kamu bisa pakai COUNT dan LENGTH.
+
+# Segini dulu tentang array, karena kepanjangan:D hehe, Saya akan buat EP 2 nya. Jadi, Akhir kata
+
+# Semoga Tidak Pusing dan,
+
+# Semoga ilmunya bermanfaat.
