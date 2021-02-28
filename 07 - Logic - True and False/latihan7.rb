8a95bf7ea149e0ab6ce09c1852ed8d5ceb0c50f5
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 19:54:16 +0700

    Create latihan7.rb

---
+++ 07 - Logic - True and False/latihan7.rb
@@ -0,0 +1,21 @@
+# Latihan 7 - Logic Boolean - True || False
+
+# Sama seperti program lainnya, Bool/Boolean dipakai ketika kita ingin tahu. Apakah Nilai ini True? Apakah Nilai ini False?
+
+# Dan, True itu Bernilai 1, Kalau False Bernilai 0.
+
+# '==' Apakah nilai kiri sama dengan nilai kanan?
+
+# '!=' Apakah nilai kiri tidak sama dengan nilai kanan?
+
+# '>, >=' Lebih Besar, Lebih Besar Dari
+
+# '<, <=' Lebih Kecil, Lebih Kecil Sama Dengan
+
+# '&&' Menggabungkan 2 Pernyataan / Dan
+
+# '||' Atau
+
+# 'True' Benar
+
+# 'False' Salah
