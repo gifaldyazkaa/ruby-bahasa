de42caaebed597ce8d9b07c35c21313abd7c31fa
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-02-28 20:15:13 +0700

    Create Congratulation.rb

---
+++ 19 - Final (End)/Congratulation.rb
@@ -0,0 +1,45 @@
+# 19 - End of Belajar Ruby Dasar
+
+# Heyo Bro, and Pak:D Selamat pak, Sudah Berhasil Melewati Rintangan Belajar Dasar Ruby ini Dari EP 1 - 17.
+
+# Pasti dari bro n bro sekalian ada yang merasa kepusingan, karena wajar aja hehe, Saya gak buat Tutorial nya di YouTube, Karena saya males mengeluarkan suara:D
+
+# Tapi Kalian udah saya suruh untuk menanyakan yang bagian gak dimengerti nya di Pull Request.
+
+# Dan gimana pak rasanya ketika udah beres belajar ini? Bisa dimengerti, Kurang ngerti, atau Ada yang Kurang?
+
+# Eksekusi aja file nya pak, dan jawab di form yang saya siapkan. xD
+
+# Setelah dasar ini, Kalian juga jangan menganggap saya sudah bisa, Tapi Belajar Lebih Banyak Lagi di Versi OOP (Object Oriented Programming).
+
+# Yang selama ini kalian pelajari gak akan sia-sia, Semuanya akan terpakai di OOP nanti. Jadi kalau menunggu Tutorial Versi OOP nya, Insyaallah Akan Saya Buat mendatang.
+
+# Terima Kasih Banyak Bagi kalian yang sudah mengikuti Pembelajaran Ruby ini dari Awal - Akhir. Dan jangan lupa buat Kasih Star di Repo Ini.
+
+# Tentunya, Ane sendiri gak akan menyediakan hanya 1 Tutorial Saja di GitHub, Tetapi, yang lainnya menyusul dan ditunggu aja pak keluaran Tutorial Selanjutnya.
+
+# Terima Kasih dan Semoga Ilmu Selama ini Bermanfaat.
+
+# --- FORM ---
+
+print "Bagaimana pak Perasaan nya Ketika Sudah Beres Sampai Akhir? (Senang, Gak Ngerti, Ada yang Kurang) : "
+
+jawaban = gets.chomp
+
+case jawaban
+
+when "Senang" || "senang"
+
+    puts "[system32.exe] Terima Kasih Banyak Pak, Semoga Ilmu yang Dipelajari Bisa Bermanfaat."
+
+when "Gak Ngerti" || "gak ngerti"
+
+    puts "[system32.exe] Bagian mana yang tidak dimengerti pak? Ajukan di Pull Request saja ya pak:)"
+
+when "Ada yang Kurang" || "Ada Yang Kurang" || "ada yang kurang" || "Kurang Sesuatu"
+
+    puts "[system32.exe] Ada yang kurang Sesuatu di Tutorial Ini? Mohon Ajukan Laporan di Pull Request ya pak:)"
+
+end
+
+# Thanks ^_^ :)
